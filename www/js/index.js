c7943b376ed15c9cb143293e5d1e9ce4168f4022
michaelmarkussen <michael-markussen@live.no>
2017-06-08 16:30:52 +0200

    Guitar tuner

---
+++ www/js/index.js
@@ -0,0 +1,124 @@
+
+function playE() {
+    var audio = document.getElementById("audioOne");
+    audio.play();
+}
+
+function playA() {
+    var audio = document.getElementById("audioTwo");
+    audio.play();
+}
+
+function playD() {
+    var audio = document.getElementById('audioThree');
+    audio.play();
+}
+
+
+
+function playG() {
+    var audio = document.getElementById("audioFour");
+    audio.play();
+}
+
+
+function playB() {
+    var audio = document.getElementById("audioFive");
+    audio.play();
+}
+
+
+function playElow() {
+    var audio = document.getElementById("audioSix");
+    audio.play();
+}
+
+function playDROPD() {
+        var audio = document.getElementById("audioOneD");
+        audio.play();
+    }
+
+
+function playOpenA() {
+    var audio = document.getElementById("audioTwoA");
+    audio.play();
+}
+
+
+function playOpenD() {
+    var audio = document.getElementById("audioThreeD");
+    audio.play();
+}
+
+function playC() {
+    var audio = document.getElementById("opencOne");
+    audio.play();
+}
+
+function playGtwo() {
+    var audio = document.getElementById("opencTwo");
+    audio.play();
+}
+
+
+function playCthree() {
+    var audio = document.getElementById("opencThree");
+    audio.play();
+}
+
+function playCfour() {
+    var audio = document.getElementById("opencfour");
+    audio.play();
+}
+
+function playCfive() {
+    var audio = document.getElementById("opencfive");
+    audio.play();
+}
+
+function playDFfour() {
+    var audio = document.getElementById("opendF");
+    audio.play();
+}
+
+function playOpenDA() {
+    var audio = document.getElementById("openDA");
+    audio.play();
+}
+
+
+function playOpenDsix() {
+    var audio = document.getElementById("audiosixd");
+    audio.play();
+}
+
+function playEmorkdown() {
+    var audio = document.getElementById("audioEmorkb");
+    audio.play();
+}
+
+function playAb() {
+    var audio = document.getElementById("Ab");
+    audio.play();
+}
+
+function playDb() {
+    var audio = document.getElementById("Db");
+    audio.play();
+}
+
+
+function playGb() {
+    var audio = document.getElementById("Gb");
+    audio.play();
+}
+
+function playBb() {
+    var audio = document.getElementById("Bb");
+    audio.play();
+}
+
+function playEblys() {
+    var audio = document.getElementById("audioElysb");
+    audio.play();
+}
